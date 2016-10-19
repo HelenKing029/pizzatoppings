aeec785d86eb4183b58087158982d79230d8a55a
Helen King <Heiskanen@Petris-MacBook-Pro.local>
2016-10-19 06:36:12 -0700

    updates

--- pizza.js
+++ pizza.js
@@ -1,47 +1,56 @@
-var toppings = ["pineapple", "pepper", "mushrooms", "olives", "pepperoni" ] 
-var prices = [1, 2, 2, 1, 2]
+var toppings = {
+    pineapple: 1,
+    pepper: 1,
+    mushroom: 2, 
+    olives: 1,
+    pepperoni: 2, 
+    ham: 2
+}
+// idea from http://www.codereadability.com/replacing-if-statements-with-object-lookups/
+var price = toppings[userselected];
 
 // from http://jsfiddle.net/y9wL27y0/
-var inputArray = [];
+//the user selects their topping
+var userselected = [];
+console.log(userselected)
+// store toppings into array 
+var userorder = [];
+userorder.push(userselected);
+console.log(userorder)
+// pass element through array
+
+// price starts at 0
+var totalprice = 0;
+ function calcTotal (inputbox){
+ var total = [];
+  //add the variables
+  return a + b; 
+}
+
+//The program should take the toppings and 
+
+//present the toppings with a total price. 
+  // store users selections in an array
+    // sum the array
+      var sum = [userselected].reduce(add, 0);
+
+      function add(a, b) {
+          return a + b;
+      }
+      console.log(sum); 
+
 var input = document.getElementById('inputbox');
 var screen = document.getElementById('screen');
-//input box to enter toppings separated by comma
 
-
-// submit button - prepare order
+//display the price and toppings selected
 
 document.getElementById('prepare').onclick = function () {
-    inputArray.push(input.value);
+    userselected.push(input.value);
     screen.innerHTML = input.value;
+    //statement to be displayed on click
+    document.getElementById("statement").innerHTML = "You have selected:" + userorder + " at a cost of $" + sum
 };
 
-// store into array 
-// pass element through array
-// get price
-
-// list element and price
-// display user selections with price
-function orderform() {
-    if (inputArray == "pineapple"){
-      console.log("pineapple")
-      total =+ 1; 
-    };
-    if (inputArray == "pepper"){
-      total =+ 2; 
-    };
-    if 
-
-}
-
-                     
-var total = 0; 
-
-for (var i = 0; i < meow.length; i++) {
-    for (var j = 0; j < toppings.length; j++) {
-        if (meow[i] == toppings[j]) 
-            total = total + price[j];
-    }
-}
 // place order button
   //popup window "thanks"
 
